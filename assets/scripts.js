40b6b4c72ec185244f13d4c1f0562a42a42bbce2
Obrienjames34 <Obrienjames934@gmail.com>
2022-05-15 14:58:07 -0400

    added code

--- assets/scripts.js
+++ assets/scripts.js
@@ -62,3 +62,88 @@ btnScore.addEventListener("click" , function(){
     scorePage(name, count)
 });
 // Time set
+
+function gametime(){
+
+    var timeinterval = setInterval(function(){
+        timer.innerText = count
+         count--;
+        }, 1000);
+
+}
+
+function scorePage(a, b) {
+
+    var userData = {
+        inits: a,
+        userScore: b
+    };
+    allScores.push(userData);
+
+    localStorage.setItem("userData", JSON.stringify(allScores));
+    location.href = "score.html";
+}
+
+function displayQuestion(question){
+    titleitem.innerText=question.title
+    question.choices.forEach(element => {
+     var button =document.createElement("button")
+    button.className="btn-primary btn-block text-left"
+    button.innerText=element
+    // questionanswers.innerHTML=""
+    questionanswers.appendChild(button)
+    button.addEventListener("click", displaynextQuestion)
+    });
+}
+
+
+function displaynextQuestion(e){
+    currentindex++
+    if(currentindex < questions.length){
+        correction(e.target.innerText == nextQuestions.answer)
+        questionanswers.innerHTML=""
+        if(currentindex < questions.length){    
+            nextQuestions= questions[currentindex]
+            displayQuestion(nextQuestions)  
+        }else {
+            currentindex = 0
+            displayQuestion(nextQuestions)  
+        }
+
+    }else{
+        console.log("endgame")
+        endgame()
+        
+
+    }
+    
+     
+}
+function correction(response){
+    
+    if(response){
+        alert.innerText= "Good"
+        console.log("Good")
+    }else {
+        alert.innerText="Wrong"
+        count = count -15
+        timer.innerHTML = count
+        console.log("Wrong")
+
+    }
+    setTimeout(function(){
+        alert.innerText=""
+    
+        }, 1000);
+
+}
+ function endgame (){
+    // btnStart.classList.add("d-none")
+    myScore.innaText = count
+    addscore.classList.remove("d-none")
+    timecounter.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+    addscore.classList.remove("d-none")
+
+
+ }
